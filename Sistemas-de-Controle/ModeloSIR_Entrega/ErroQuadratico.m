14bf366dc8b4a2e30fcebe6c679d576d41bc3f66
Enrico <rico500000@gmail.com>
2022-08-31 16:11:15 -0300

    Organizando

--- Sistemas-de-Controle/ModeloSIR_Entrega/ErroQuadratico.m
+++ Sistemas-de-Controle/ModeloSIR_Entrega/ErroQuadratico.m
@@ -1,10 +1,12 @@
-function [Eq] = ErroQuadraticoSIR(X,Rd,Id)
+function [Eq] = ErroQuadraticoSIR(X,Sd,Id,Rd)
 % Esta função determina o erro quadrático
 % entre a saída real e a saída do modelo.
 % u - entrada real
 % y - saída real
 % t - tempo real
 % X = [Km pm];
+global beta;
+global r;
 
 beta = X(1);
 r = X(2);
@@ -18,20 +20,22 @@ i0 = 1;
 r0 = 0;
 s0 = N - i0;
 
-options = simset('solver','ode45','MaxStep',0.01,'InitialStep',0.01,'ReturnWorkspaceOutputs', 'on');
-y = sim("SimulacaoSIR.slx",1000,options);
+options = simset('solver','ode45','MaxStep',0.1,'InitialStep',0.1,'ReturnWorkspaceOutputs', 'on');
+simOut = sim("SimulacaoSIR.slx",1000,options);
 
 %%% Saida do modelo
 
-I = y.logsout{1}.Values.Data; % Saída I
-R = y.logsout{2}.Values.Data; % Saída R
+S = simOut.logsout{1}.Values.Data;  
+I = simOut.logsout{2}.Values.Data;
+R = simOut.logsout{3}.Values.Data;
 
+S = resample(S,length(S),length(Sd));
 I = resample(I,length(I),length(Id));
 R = resample(R,length(R),length(Rd));
 
 %%% Erro quadrático
 
-Eq = sum((I - Id).^2) + sum((R - Rd).^2); %% cálculo do erro
+Eq = sum((I - Id).^2) + sum((R - Rd).^2) + sum((S - Sd).^2) %% cálculo do erro
 
 % plot(t,y,t,ym,"linewidth", 2)
 
